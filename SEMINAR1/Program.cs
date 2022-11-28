24437bc8485a7f517388c30b6900c610a57fded6
Natali <leshuc2014@ya.ru>
2022-11-28 10:21:38 +0300

    1

---
+++ SEMINAR1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());//ввод чисел;
+int i = n * (-1);
+while (i <= n)
+{
+    Console.Write($"{i}  ");
+    i = i+ 1;// i++;
+}
