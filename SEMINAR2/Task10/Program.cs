2afc477911a1b1feaedc6cda1b71450539b27fbc
Natali <leshuc2014@ya.ru>
2022-11-30 13:20:19 +0300

    Dom SEMINAR2

---
+++ SEMINAR2/Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int number = ReadInt("Введите трёхзначное число ");
+int amout = number.ToString().Length;
+if (amout < 3 || amout > 3)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число ");
+}
+else
+{
+    Console.WriteLine(IntCentr(number));
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+int IntCentr(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
---
+++ SEMINAR2/Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ if (a >= 1 && a <= 7)
+ {
+    if (a == 6 | a == 7)Console.WriteLine("Сегодня выходной день ");
+    else Console.WriteLine("Сегодня рабочий день ");
+ }
+ else Console.WriteLine("Не верное число ");
+
