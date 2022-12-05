325448844851a28fcd12435e78b9ccf20ee2007a
Natali <leshuc2014@ya.ru>
2022-12-05 12:04:19 +0300

    Zadaniya Seminar4

---
+++ SEMINAR4/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Console.Clear();
+Console.WriteLine("Введите число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Резудьтат: {(n + 1)/ 2 * n}");
+*/
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result =1;
+for (int i = 1; i <= n; i++)
+     result *=i;
+Console.WriteLine($"Результат: {result}");
---
+++ SEMINAR4/Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+int num = a;
+for (int i = 1;a < b; i++);
+{
+    num = num * a;
+}
+Console.WriteLine("a в степени b равно: + num");
