951bc61d00ee424e91c5ef48f8d827f3fd718c59
Natali <leshuc2014@ya.ru>
2022-12-14 12:26:15 +0300

    DZ SEMINAR7

---
+++ SEMINAR7/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputMatrix(int[,] matrix);
+{
+    for (int i= 0; i< matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <matrix.GetLength(1); j++)
+         {
+            matrix[i,j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+         }
+         Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("введите размеры матрици: ");
+string[] numbers = Console.ReadLine().Split("  ");
+int[,] matrix = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
