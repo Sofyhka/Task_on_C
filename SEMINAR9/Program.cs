4766a4c4871cd956d039e42ec3dfeb9ef7683c91
Natali <leshuc2014@ya.ru>
2022-12-20 22:43:33 +0300

    SEMINAR9

---
+++ SEMINAR9/Program.cs
@@ -0,0 +1,31 @@
+﻿
+/*
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x == 0)
+        return 0;
+    return sum() + x;
+}
+
+Console.Clear();
+Console.WriteLine(sum());
+*/
+
+void rec(int n)
+{
+    if (n == 1)
+        Console.Write(n + " ");
+    if (n > 1)
+    {    
+        rec(n - 1);
+        Console.Write(n + " ");
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+rec(n);
+
+
